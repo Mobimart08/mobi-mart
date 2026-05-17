b8e53db8fc9516651412d6ab22d0e727de979bac
Hitanshu Jhorar <hitanshujhorar@gmail.com>
2026-05-17 17:34:46 +0530

    ....

--- src/storefront/storefrontProductsContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const StorefrontProductsContext = createContext(null);
-
-export default StorefrontProductsContext;
--- src/utils/currency.js
+++
@@ -1,7 +0,0 @@
-export const formatINR = (price) =>
-  new Intl.NumberFormat("en-IN", {
-    style: "currency",
-    currency: "INR",
-    maximumFractionDigits: 0,
-  }).format(Number(price) || 0);
-
